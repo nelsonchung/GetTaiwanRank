280265e934dfc7f8d58f1c463903850fe3c8157f
nelsonchung <chihchun.chung@gmail.com>
2016-07-15 15:09:38 +0800

    問題 1. 會遞迴跑出很多次 2. 取不出幣值名稱

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from lxml import html
 
 #not really fix utf8 problem
 import sys
@@ -16,9 +17,39 @@ url = 'http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm'
 r = requests.get(url)
 #requests.setCharacterEncoding("UTF-8");
 #r.text.encode("utf-8")
-print r.text
+#print r.text
 
 #save to file
-data = open('data.html', 'w')
-data.write(r.text)
-data.close()
+#data = open('data.html', 'w')
+#data.write(r.text)
+#data.close()
+
+#Parse Pages
+#tree = html.fromstring(r.text)
+tree = html.fromstring(r.content)
+
+#for table in tree.xpath('//table[2]'):
+#for table in tree.xpath('//table[1]'):
+#table = tree.xpath('//table[2]')
+#print table
+#fail
+#for tr in table.xpath('//tbody/tr'):
+#fail
+#for tr in tree.xpath('//table[2]/tbody/tr'):
+#for tr in tree.xpath('//table[2]/tr'):
+#ok
+#for tr in tree.xpath('//table[1]/tr'):
+for tr in tree.xpath('//table[2]/tr'):
+    ##print tr
+    for td in tr.xpath('//td[@class="titleLeft"]'):
+        print "============================="
+        print td.text
+        #for img in td.xpath('//img[@class="paddingLeft16"]'):
+        #	print img.text
+        print "============================="
+    	for td in tr.xpath('//td[@class="decimal"]'):
+        	print "*****************************"
+        	print td.text
+        	print "*****************************"
+
+
